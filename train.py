515a0d6dd4d85da88f17a6e3d82d2cf6701f979b
jialanxin <jialanxin1996@hotmail.com>
2020-12-21 11:05:33 +0800

    split train/validate set

--- train.py
+++ train.py
@@ -1,5 +1,5 @@
 from dataset import StructureRamanDataset
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, random_split
 import pickle
 import json
 from model import MegNet
@@ -17,41 +17,53 @@ with open("Raman_encoded_JVASP_90000.json","r") as f:
 
 device = torch.device("cuda")
 dataset = StructureRamanDataset(structures,ramans)
-data_loader = DataLoader(dataset=dataset,batch_size=200,collate_fn=collate_fn,num_workers=5,shuffle=True)
+dataset_length = len(dataset)
+train_set, validate_set = random_split(dataset,[int(0.8*dataset_length)+1,int(0.2*dataset_length)])
+train_dataloader = DataLoader(dataset=train_set,batch_size=200,collate_fn=collate_fn,num_workers=5,shuffle=True)
+validate_dataloader = DataLoader(dataset=validate_set,batch_size=200,collate_fn=collate_fn,num_workers=5,shuffle=True)
 net = MegNet()
 net.to(device)
 
 loss_func = torch.nn.MSELoss()
-optimizer = torch.optim.Adam(net.parameters(),lr=1e-4)
-checkpoint = torch.load("./checkpoint_epoch_1501_loss_0.014194694575336245.pkl")
-net.load_state_dict(checkpoint["model_state_dict"])
-optimizer.load_state_dict(checkpoint["optimizer_state_dict"])
-start_epoch = checkpoint["epoch"]
+optimizer = torch.optim.Adam(net.parameters())
+
 
 from torch.utils.tensorboard import SummaryWriter
 writer = SummaryWriter()
 
-for epoch in range(start_epoch+1,2002):
+for epoch in range(0,1000):
     start = torch.cuda.Event(enable_timing=True)
     end   = torch.cuda.Event(enable_timing=True)
     start.record()
-    accumulate_loss = 0.0
-    for i ,data in enumerate(data_loader):
+    train_loss = 0.0
+    for i ,data in enumerate(train_dataloader):
         atoms,state,bonds,bond_atom_1,bond_atom_2,atoms_mark,bonds_mark,ramans = data
+        net.train()
+        optimizer.zero_grad()
         predicted_spectrum = net(atoms.to(device),state.to(device),bonds.to(device),bond_atom_1.to(device),bond_atom_2.to(device),atoms_mark.to(device),bonds_mark.to(device))
         loss = loss_func(predicted_spectrum,ramans.to(device))
         loss.backward()
-        accumulate_loss += loss.item()
+        train_loss += loss.item()
         optimizer.step()
-        optimizer.zero_grad()
-    accumulate_loss = accumulate_loss/i
+        
+    train_loss = train_loss/i
+
+    validate_loss = 0.0
+    for i,data in enumerate(validate_dataloader):
+        atoms,state,bonds,bond_atom_1,bond_atom_2,atoms_mark,bonds_mark,ramans = data
+        net.eval()
+        predicted_spectrum = net(atoms.to(device),state.to(device),bonds.to(device),bond_atom_1.to(device),bond_atom_2.to(device),atoms_mark.to(device),bonds_mark.to(device))
+        loss = loss_func(predicted_spectrum,ramans.to(device))
+        validate_loss += loss.item()
+    validate_loss = validate_loss/i
     end.record()
     torch.cuda.synchronize()
     print(f"epoch:{epoch}")
     print(f"Time:{start.elapsed_time(end)}")
-    print(f"Loss:{accumulate_loss}")
-    writer.add_scalar("Loss vs Epoch",accumulate_loss,epoch)
+    print(f"train loss:{train_loss}")
+    print(f"validate loss:{validate_loss}")
+    writer.add_scalars("Train and Validate Loss",{"train_loss":train_loss,"validate_loss":validate_loss},epoch)
     if epoch % 100 == 1:
-        checkpoint = {"model_state_dict":net.state_dict(),"optimizer_state_dict":optimizer.state_dict(),"epoch":epoch,"loss":accumulate_loss}
-        torch.save(checkpoint,f"./checkpoint_epoch_{epoch}_loss_{accumulate_loss}.pkl")
+        checkpoint = {"model_state_dict":net.state_dict(),"optimizer_state_dict":optimizer.state_dict(),"epoch":epoch,"loss":validate_loss}
+        torch.save(checkpoint,f"./checkpoint_epoch_{epoch}_val_loss_{validate_loss}.pkl")
 writer.close()
---
+++ train_overfit.py
@@ -0,0 +1,57 @@
+from dataset import StructureRamanDataset
+from torch.utils.data import DataLoader
+import pickle
+import json
+from model import MegNet
+import torch
+import time
+from dataloader import collate_fn
+
+with open("Structures.pkl","rb") as f:
+    structures = pickle.load(f)
+
+with open("Raman_encoded_JVASP_90000.json","r") as f:
+    ramans = json.loads(f.read())
+
+
+
+device = torch.device("cuda")
+dataset = StructureRamanDataset(structures,ramans)
+data_loader = DataLoader(dataset=dataset,batch_size=200,collate_fn=collate_fn,num_workers=5,shuffle=True)
+net = MegNet()
+net.to(device)
+
+loss_func = torch.nn.MSELoss()
+optimizer = torch.optim.Adam(net.parameters(),lr=1e-4)
+checkpoint = torch.load("./checkpoint_epoch_1901_loss_0.013536745289133654.pkl")
+net.load_state_dict(checkpoint["model_state_dict"])
+optimizer.load_state_dict(checkpoint["optimizer_state_dict"])
+start_epoch = checkpoint["epoch"]
+
+from torch.utils.tensorboard import SummaryWriter
+writer = SummaryWriter()
+
+for epoch in range(start_epoch+1,3002):
+    start = torch.cuda.Event(enable_timing=True)
+    end   = torch.cuda.Event(enable_timing=True)
+    start.record()
+    accumulate_loss = 0.0
+    for i ,data in enumerate(data_loader):
+        atoms,state,bonds,bond_atom_1,bond_atom_2,atoms_mark,bonds_mark,ramans = data
+        predicted_spectrum = net(atoms.to(device),state.to(device),bonds.to(device),bond_atom_1.to(device),bond_atom_2.to(device),atoms_mark.to(device),bonds_mark.to(device))
+        loss = loss_func(predicted_spectrum,ramans.to(device))
+        loss.backward()
+        accumulate_loss += loss.item()
+        optimizer.step()
+        optimizer.zero_grad()
+    accumulate_loss = accumulate_loss/i
+    end.record()
+    torch.cuda.synchronize()
+    print(f"epoch:{epoch}")
+    print(f"Time:{start.elapsed_time(end)}")
+    print(f"Loss:{accumulate_loss}")
+    writer.add_scalar("Loss vs Epoch",accumulate_loss,epoch)
+    if epoch % 100 == 1:
+        checkpoint = {"model_state_dict":net.state_dict(),"optimizer_state_dict":optimizer.state_dict(),"epoch":epoch,"loss":accumulate_loss}
+        torch.save(checkpoint,f"./checkpoint_epoch_{epoch}_loss_{accumulate_loss}.pkl")
+writer.close()
