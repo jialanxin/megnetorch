fae0278d85cafc20f8b29449c8874bcd52d3e4e5
jialanxin <jialanxin1996@hotmail.com>
2020-12-24 14:18:49 +0800

    config num megnetblocks

--- model.py
+++ model.py
@@ -3,12 +3,18 @@ import torch.nn
 from torch.nn import Embedding
 from torch_geometric.nn import Set2Set
 
+
 def ff(input_dim):
-    return torch.nn.Sequential(torch.nn.Linear(input_dim,64),torch.nn.RReLU(),torch.nn.Linear(64,32),torch.nn.Dropout(),torch.nn.RReLU())
+    return torch.nn.Sequential(torch.nn.Linear(input_dim, 64), torch.nn.RReLU(), torch.nn.Linear(64, 32), torch.nn.Dropout(), torch.nn.RReLU())
+
+
 def fff(input_dim):
-    return torch.nn.Sequential(torch.nn.Linear(input_dim,64),torch.nn.RReLU(),torch.nn.Linear(64,64),torch.nn.RReLU(),torch.nn.Dropout(),torch.nn.Linear(64,32),torch.nn.RReLU())
-def ff_output(input_dim,output_dim):
-    return torch.nn.Sequential(torch.nn.Linear(input_dim,128),torch.nn.RReLU(),torch.nn.Linear(128,64),torch.nn.RReLU(),torch.nn.Dropout(),torch.nn.Linear(64,output_dim))
+    return torch.nn.Sequential(torch.nn.Linear(input_dim, 64), torch.nn.RReLU(), torch.nn.Linear(64, 64), torch.nn.RReLU(), torch.nn.Dropout(), torch.nn.Linear(64, 32), torch.nn.RReLU())
+
+
+def ff_output(input_dim, output_dim):
+    return torch.nn.Sequential(torch.nn.Linear(input_dim, 128), torch.nn.RReLU(), torch.nn.Linear(128, 64), torch.nn.RReLU(), torch.nn.Dropout(), torch.nn.Linear(64, output_dim))
+
 
 class MegNetLayer(torch.nn.Module):
     def __init__(self) -> None:
@@ -16,52 +22,71 @@ class MegNetLayer(torch.nn.Module):
         self.phi_e = fff(128)
         self.phi_v = fff(96)
         self.phi_u = fff(96)
-        
-    def forward(self,bonds,bond_atom_1,bond_atom_2,atoms,state):
+
+    def forward(self, bonds, bond_atom_1, bond_atom_2, atoms, state):
         sum_of_num_bonds = bonds.shape[0]
         atom_info = atoms.shape[1]
         # for ix_bond in range(sum_of_num_bonds):
         #     for res in range(atom_info):
         #        atom_1_to_bonds[ix_bond,res] =atoms[bond_atom_1[ix_bond,res],res]
-        bond_atom_1 = bond_atom_1.unsqueeze(dim=1).repeat((1,atom_info))  # (sum_of_num_bonds,atom_info)
-        bond_atom_2 = bond_atom_2.unsqueeze(dim=1).repeat((1,atom_info))  # (sum_of_num_bonds,atom_info)
-        atom_1_to_bonds = torch.gather(input=atoms,dim=0,index=bond_atom_1)  #(sum_of_num_bonds,atom_info)
-        atom_2_to_bonds = torch.gather(input=atoms,dim=0,index=bond_atom_2)  #(sum_of_num_bonds,atom_info)
-        bonds = torch.cat((atom_1_to_bonds,atom_2_to_bonds,bonds,state.repeat((sum_of_num_bonds,1))),dim=1) #(sum_of_num_bonds,atom_info*2+bond_info+state_info)
-        bonds = self.phi_e(bonds) #(sum_of_num_bonds,bond_info)
+        bond_atom_1 = bond_atom_1.unsqueeze(dim=1).repeat(
+            (1, atom_info))  # (sum_of_num_bonds,atom_info)
+        bond_atom_2 = bond_atom_2.unsqueeze(dim=1).repeat(
+            (1, atom_info))  # (sum_of_num_bonds,atom_info)
+        # (sum_of_num_bonds,atom_info)
+        atom_1_to_bonds = torch.gather(input=atoms, dim=0, index=bond_atom_1)
+        # (sum_of_num_bonds,atom_info)
+        atom_2_to_bonds = torch.gather(input=atoms, dim=0, index=bond_atom_2)
+        bonds = torch.cat((atom_1_to_bonds, atom_2_to_bonds, bonds, state.repeat(
+            (sum_of_num_bonds, 1))), dim=1)  # (sum_of_num_bonds,atom_info*2+bond_info+state_info)
+        bonds = self.phi_e(bonds)  # (sum_of_num_bonds,bond_info)
 
-        bonds_to_atoms = torch.zeros_like(atoms) #(sum_of_num_atoms,bond_info) here because bond_info and atom_info are both 32
+        # (sum_of_num_atoms,bond_info) here because bond_info and atom_info are both 32
+        bonds_to_atoms = torch.zeros_like(atoms)
         sum_of_num_atoms = atoms.shape[0]
-        count_bonds_to_atoms = torch.zeros_like(atoms) #(sum_of_num_atoms,bond_info)
+        count_bonds_to_atoms = torch.zeros_like(
+            atoms)  # (sum_of_num_atoms,bond_info)
         # for ix_bond in range(sum_of_num_bonds):
         #     for res in range(bond_info):
         #         bonds_to_atoms[bond_atom_1[ix_bond,res],res] += bonds[ix_bond,res]
         #         count_bonds_to_atoms[bond_atom_1[ix_bond,res],res]+=1
-        bonds_to_atoms = bonds_to_atoms.scatter_add(dim=0,index=bond_atom_1,src=bonds)  
-        count_bonds_to_atoms = count_bonds_to_atoms.scatter_add(dim=0,index=bond_atom_1,src=torch.ones_like(bonds))
-        bonds_to_atoms = bonds_to_atoms/count_bonds_to_atoms #(sum_of_num_atoms,bond_info)
-        atoms = torch.cat((bonds_to_atoms,atoms,state.repeat((sum_of_num_atoms,1))),dim=1) #(sum_of_num_atoms,bond_info+atom_info+state_info)
-        atoms = self.phi_v(atoms) #(sum_of_num_atoms,atom_info)
+        bonds_to_atoms = bonds_to_atoms.scatter_add(
+            dim=0, index=bond_atom_1, src=bonds)
+        count_bonds_to_atoms = count_bonds_to_atoms.scatter_add(
+            dim=0, index=bond_atom_1, src=torch.ones_like(bonds))
+        # (sum_of_num_atoms,bond_info)
+        bonds_to_atoms = bonds_to_atoms/count_bonds_to_atoms
+        # (sum_of_num_atoms,bond_info+atom_info+state_info)
+        atoms = torch.cat(
+            (bonds_to_atoms, atoms, state.repeat((sum_of_num_atoms, 1))), dim=1)
+        atoms = self.phi_v(atoms)  # (sum_of_num_atoms,atom_info)
 
-        bonds_to_state = torch.mean(bonds,dim=0,keepdim=True) # (1,bond_info)
-        atoms_to_state = torch.mean(atoms,dim=0,keepdim=True) # (1,atom_info)
-        state = torch.cat((bonds_to_state,atoms_to_state,state),dim=1)  #(1,bond_info+atom_info+state_info)
-        state = self.phi_u(state)  #(1,state_info)
+        bonds_to_state = torch.mean(
+            bonds, dim=0, keepdim=True)  # (1,bond_info)
+        atoms_to_state = torch.mean(
+            atoms, dim=0, keepdim=True)  # (1,atom_info)
+        # (1,bond_info+atom_info+state_info)
+        state = torch.cat((bonds_to_state, atoms_to_state, state), dim=1)
+        state = self.phi_u(state)  # (1,state_info)
         return bonds, atoms, state
+
 
 class FirstMegnetBlock(torch.nn.Module):
     def __init__(self) -> None:
         super().__init__()
         self.megnetlayer = MegNetLayer()
-    def forward(self,bonds,bond_atom_1,bond_atom_2,atoms,state):
+
+    def forward(self, bonds, bond_atom_1, bond_atom_2, atoms, state):
         residual_bonds = bonds.clone()
         residual_atoms = atoms.clone()
         residual_state = state.clone()
-        residual_bonds ,residual_atoms, residual_state = self.megnetlayer(residual_bonds,bond_atom_1,bond_atom_2,residual_atoms,residual_state)
+        residual_bonds, residual_atoms, residual_state = self.megnetlayer(
+            residual_bonds, bond_atom_1, bond_atom_2, residual_atoms, residual_state)
         atoms = atoms + residual_atoms
         bonds = bonds + residual_bonds
-        state  = state + residual_state
+        state = state + residual_state
         return bonds, atoms, state
+
 
 class FullMegnetBlock(torch.nn.Module):
     def __init__(self) -> None:
@@ -70,43 +95,56 @@ class FullMegnetBlock(torch.nn.Module):
         self.atoms_ff = ff(32)
         self.bonds_ff = ff(32)
         self.state_ff = ff(32)
-    def forward(self,bonds,bond_atom_1,bond_atom_2,atoms,state):
+
+    def forward(self, bonds, bond_atom_1, bond_atom_2, atoms, state):
         residual_bonds = bonds.clone()
         residual_atoms = atoms.clone()
         residual_state = state.clone()
         residual_bonds = self.bonds_ff(residual_bonds)
         residual_atoms = self.atoms_ff(residual_atoms)
         residual_state = self.state_ff(residual_state)
-        residual_bonds,residual_atoms,residual_state = self.megnetlayer(residual_bonds,bond_atom_1,bond_atom_2,residual_atoms,residual_state)
+        residual_bonds, residual_atoms, residual_state = self.megnetlayer(
+            residual_bonds, bond_atom_1, bond_atom_2, residual_atoms, residual_state)
         atoms = atoms + residual_atoms
         bonds = bonds + residual_bonds
-        state  = state + residual_state
-        return bonds, atoms, state 
+        state = state + residual_state
+        return bonds, atoms, state
+
 
 class MegNet(torch.nn.Module):
-    def __init__(self) -> None:
+    def __init__(self, num_of_megnetblock) -> None:
         super().__init__()
-        self.atomic_embedding = Embedding(95,16)
+        self.atomic_embedding = Embedding(95, 16)
         self.atom_preblock = ff(16)
         self.bond_preblock = ff(100)
         self.state_preblock = ff(2)
         self.firstblock = FirstMegnetBlock()
-        self.fullblocks = torch.nn.ModuleList([FullMegnetBlock() for i in range(3)])
-        self.set2set_e = Set2Set(in_channels=32,processing_steps=3)
-        self.set2set_v = Set2Set(in_channels=32,processing_steps=3)
-        self.output_layer = ff_output(input_dim=160,output_dim=200)
+        self.fullblocks = torch.nn.ModuleList(
+            [FullMegnetBlock() for i in range(num_of_megnetblock)])
+        self.set2set_e = Set2Set(in_channels=32, processing_steps=3)
+        self.set2set_v = Set2Set(in_channels=32, processing_steps=3)
+        self.output_layer = ff_output(input_dim=160, output_dim=200)
 
-    def forward(self, atoms,state,bonds,bond_atom_1,bond_atom_2,batch_mark_for_atoms,batch_mark_for_bonds):
-        atoms_embedded = self.atomic_embedding(atoms) #(sum_of_num_atoms,atom_info)
-        atoms = self.atom_preblock(atoms_embedded)    #(sum_of_num_atoms,atom_info)
-        bonds = self.bond_preblock(bonds)             #(sum_of_num_bonds,bond_info)
-        state = self.state_preblock(state)            #(1,state_info)
-        bonds,atoms,state = self.firstblock(bonds,bond_atom_1,bond_atom_2,atoms,state)
+    def forward(self, atoms, state, bonds, bond_atom_1, bond_atom_2, batch_mark_for_atoms, batch_mark_for_bonds):
+        atoms_embedded = self.atomic_embedding(
+            atoms)  # (sum_of_num_atoms,atom_info)
+        # (sum_of_num_atoms,atom_info)
+        atoms = self.atom_preblock(atoms_embedded)
+        bonds = self.bond_preblock(bonds)  # (sum_of_num_bonds,bond_info)
+        state = self.state_preblock(state)  # (1,state_info)
+        bonds, atoms, state = self.firstblock(
+            bonds, bond_atom_1, bond_atom_2, atoms, state)
         for block in self.fullblocks:
-            bonds,atoms,state = block(bonds,bond_atom_1,bond_atom_2,atoms,state)
+            bonds, atoms, state = block(
+                bonds, bond_atom_1, bond_atom_2, atoms, state)
         batch_size = batch_mark_for_bonds.max()+1
-        bonds = self.set2set_e(bonds,batch=batch_mark_for_bonds)  # (batch_size,bond_info)
-        atoms = self.set2set_v(atoms,batch=batch_mark_for_atoms)  # (batch_size,atom_info)
-        gather_all = torch.cat((bonds,atoms,state.repeat(batch_size,1)),dim=1) #(batch_size, bond_info+atom_info+state_info)
-        output_spectrum = self.output_layer(gather_all) #(batch_size, raman_info)
+        # (batch_size,bond_info)
+        bonds = self.set2set_e(bonds, batch=batch_mark_for_bonds)
+        # (batch_size,atom_info)
+        atoms = self.set2set_v(atoms, batch=batch_mark_for_atoms)
+        # (batch_size, bond_info+atom_info+state_info)
+        gather_all = torch.cat(
+            (bonds, atoms, state.repeat(batch_size, 1)), dim=1)
+        output_spectrum = self.output_layer(
+            gather_all)  # (batch_size, raman_info)
         return output_spectrum
--- train.py
+++ train.py
@@ -1,3 +1,4 @@
+from torch.utils.tensorboard import SummaryWriter
 from dataset import StructureRamanDataset
 from torch.utils.data import DataLoader, random_split
 import pickle
@@ -6,28 +7,41 @@ from model import MegNet
 import torch
 import time
 from dataloader import collate_fn
+import yaml
+import argparse
 
-with open("Structures.pkl","rb") as f:
+parser = argparse.ArgumentParser(description="Select a train_config.yaml file")
+parser.add_argument(dest="filename", metavar="/path/to/file")
+arg = parser.parse_args()
+path_to_file = arg.filename
+with open(path_to_file, "r") as f:
+    config = yaml.load(f.read(), Loader=yaml.BaseLoader)
+prefix = config["prefix"]
+model = config["model"]
+
+
+with open("Structures.pkl", "rb") as f:
     structures = pickle.load(f)
 
-with open("Raman_encoded_JVASP_90000.json","r") as f:
+with open("Raman_encoded_JVASP_90000.json", "r") as f:
     ramans = json.loads(f.read())
 
 
-
 device = torch.device("cuda")
-dataset = StructureRamanDataset(structures,ramans)
+dataset = StructureRamanDataset(structures, ramans)
 dataset_length = len(dataset)
-train_set, validate_set = random_split(dataset,[int(0.8*dataset_length)+1,int(0.2*dataset_length)])
-train_dataloader = DataLoader(dataset=train_set,batch_size=64,collate_fn=collate_fn,num_workers=4,shuffle=True)
-validate_dataloader = DataLoader(dataset=validate_set,batch_size=64,collate_fn=collate_fn,num_workers=4,shuffle=True)
-net = MegNet()
+train_set, validate_set = random_split(
+    dataset, [int(0.8*dataset_length)+1, int(0.2*dataset_length)])
+train_dataloader = DataLoader(
+    dataset=train_set, batch_size=64, collate_fn=collate_fn, num_workers=4, shuffle=True)
+validate_dataloader = DataLoader(
+    dataset=validate_set, batch_size=64, collate_fn=collate_fn, num_workers=4, shuffle=True)
+net = MegNet(num_of_megnetblock=int(model["num_of_megnetblock"] or 3))
 net.to(device)
 
 loss_func = torch.nn.MSELoss()
 optimizer = torch.optim.Adam(net.parameters())
-schedualer = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer,T_max=64)
-prefix = "/home/jlx/v0.2.1/4.train_RReLU_and_more_dropout/"
+schedualer = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=64)
 
 # checkpoint = torch.load(prefix+"checkpoint_epoch_101_val_loss_0.0784395659076316.pkl")
 # net.load_state_dict(checkpoint["model_state_dict"])
@@ -36,32 +50,33 @@ prefix = "/home/jlx/v0.2.1/4.train_RReLU_and_more_dropout/"
 # last_epoch = checkpoint["epoch"]
 
 
-from torch.utils.tensorboard import SummaryWriter
 writer = SummaryWriter(prefix+"runs")
 
-for epoch in range(0,2002):
+for epoch in range(0, 2002):
     start = torch.cuda.Event(enable_timing=True)
-    end   = torch.cuda.Event(enable_timing=True)
+    end = torch.cuda.Event(enable_timing=True)
     start.record()
     train_loss = 0.0
-    for i ,data in enumerate(train_dataloader):
-        atoms,state,bonds,bond_atom_1,bond_atom_2,atoms_mark,bonds_mark,ramans = data
+    for i, data in enumerate(train_dataloader):
+        atoms, state, bonds, bond_atom_1, bond_atom_2, atoms_mark, bonds_mark, ramans = data
         net.train()
         optimizer.zero_grad()
-        predicted_spectrum = net(atoms.to(device),state.to(device),bonds.to(device),bond_atom_1.to(device),bond_atom_2.to(device),atoms_mark.to(device),bonds_mark.to(device))
-        loss = loss_func(predicted_spectrum,ramans.to(device))
+        predicted_spectrum = net(atoms.to(device), state.to(device), bonds.to(device), bond_atom_1.to(
+            device), bond_atom_2.to(device), atoms_mark.to(device), bonds_mark.to(device))
+        loss = loss_func(predicted_spectrum, ramans.to(device))
         loss.backward()
         train_loss += loss.item()
         optimizer.step()
-        
+
     train_loss = train_loss/i
 
     validate_loss = 0.0
-    for i,data in enumerate(validate_dataloader):
-        atoms,state,bonds,bond_atom_1,bond_atom_2,atoms_mark,bonds_mark,ramans = data
+    for i, data in enumerate(validate_dataloader):
+        atoms, state, bonds, bond_atom_1, bond_atom_2, atoms_mark, bonds_mark, ramans = data
         net.eval()
-        predicted_spectrum = net(atoms.to(device),state.to(device),bonds.to(device),bond_atom_1.to(device),bond_atom_2.to(device),atoms_mark.to(device),bonds_mark.to(device))
-        loss = loss_func(predicted_spectrum,ramans.to(device))
+        predicted_spectrum = net(atoms.to(device), state.to(device), bonds.to(device), bond_atom_1.to(
+            device), bond_atom_2.to(device), atoms_mark.to(device), bonds_mark.to(device))
+        loss = loss_func(predicted_spectrum, ramans.to(device))
         validate_loss += loss.item()
     validate_loss = validate_loss/i
     end.record()
@@ -70,9 +85,12 @@ for epoch in range(0,2002):
     print(f"Time:{start.elapsed_time(end)}")
     print(f"train loss:{train_loss}")
     print(f"validate loss:{validate_loss}")
-    writer.add_scalars("Train and Validate Loss",{"train_loss":train_loss,"validate_loss":validate_loss},epoch)
+    writer.add_scalars("Train and Validate Loss", {
+                       "train_loss": train_loss, "validate_loss": validate_loss}, epoch)
     schedualer.step()
     if epoch % 100 == 1:
-        checkpoint = {"model_state_dict":net.state_dict(),"optimizer_state_dict":optimizer.state_dict(),"schedualer_state_dict":schedualer.state_dict(),"epoch":epoch,"loss":validate_loss}
-        torch.save(checkpoint,prefix+f"/checkpoint_epoch_{epoch}_val_loss_{validate_loss}.pkl")
+        checkpoint = {"model_state_dict": net.state_dict(), "optimizer_state_dict": optimizer.state_dict(
+        ), "schedualer_state_dict": schedualer.state_dict(), "epoch": epoch, "loss": validate_loss}
+        torch.save(checkpoint, prefix +
+                   f"/checkpoint_epoch_{epoch}_val_loss_{validate_loss}.pkl")
 writer.close()
