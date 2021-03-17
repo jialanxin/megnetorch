79eb1fd7afe68290ea1d1317ae3b74998ac48f7b
jialanxin <jialanxin1996@hotmail.com>
2021-03-17 19:57:31 +0800

    Finetune: loss weight 2

--- finetune.py
+++ finetune.py
@@ -125,7 +125,9 @@ class Experiment(pl.LightningModule):
         predicted_spectrum = self.shared_procedure(batch)
         loss = F.l1_loss(predicted_spectrum, ramans, reduction="none")
         self.log("train_loss", loss.mean(), on_epoch=True, on_step=False)
-        loss_weight = F.softmax(ramans, dim=1)
+        loss_weight = torch.pow(2,ramans)
+        weight_sum = loss_weight.sum(dim=1,keepdim=True)
+        loss_weight = loss_weight/weight_sum
         loss_weighed = torch.sum(loss*loss_weight, dim=1).mean()
         self.log("train_loss_weighed", loss_weighed,
                  on_epoch=True, on_step=False)
@@ -144,7 +146,9 @@ class Experiment(pl.LightningModule):
         predicted_spectrum = self.shared_procedure(batch)
         loss = F.l1_loss(predicted_spectrum, ramans, reduction="none")
         self.log("val_loss", loss.mean(), on_epoch=True, on_step=False)
-        loss_weight = F.softmax(ramans, dim=1)
+        loss_weight = torch.pow(2,ramans)
+        weight_sum = loss_weight.sum(dim=1,keepdim=True)
+        loss_weight = loss_weight/weight_sum
         loss_weighed = torch.sum(loss*loss_weight, dim=1).mean()
         self.log("val_loss_weighed", loss_weighed,
                  on_epoch=True, on_step=False)
