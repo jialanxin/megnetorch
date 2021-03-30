42e7400d7935a48e12be2022909863043a958b5a
jialanxin <jialanxin1996@hotmail.com>
2021-03-30 17:49:42 +0800

    Finetune: yolov1

--- finetune.py
+++ finetune.py
@@ -39,11 +39,7 @@ class Experiment(pl.LightningModule):
         self.position_embedding = spgp_model.position_embedding
         self.lattice_embedding = spgp_model.lattice_embedding
         self.encoder = spgp_model.encoder
-        encode_layer = torch.nn.TransformerEncoderLayer(
-            d_model=128, nhead=8, dim_feedforward=512)
-        self.more_encoder = torch.nn.TransformerEncoder(
-            encode_layer, num_layers=2)
-        self.readout = ff_output(input_dim=128, output_dim=25)
+        self.readout = ff_output(input_dim=128, output_dim=50)
 
     @staticmethod
     def Gassian_expand(value_list, min_value, max_value, intervals, expand_width, device):
@@ -141,100 +137,136 @@ class Experiment(pl.LightningModule):
 
         # (1+max_atoms, batch_size, atoms_info)
         atoms = self.encoder(src=atoms, src_key_padding_mask=padding_mask)
-        atoms = self.more_encoder(src=atoms, src_key_padding_mask=padding_mask)
 
         system_out = atoms[0]  # (batch_size,atoms_info)
 
-        output_spectrum = self.readout(system_out)  # (batch_size, raman_info)
-        output_spectrum = torch.exp(output_spectrum)
+        # (batch_size, wavelength_clips, confidence+position)
+        output_spectrum = self.readout(system_out).reshape(-1, 25, 2)
 
         return output_spectrum
 
     def forward(self, batch):
         predicted_spectrum = self.shared_procedure(batch)
         return predicted_spectrum
+
     @staticmethod
-    def scores(spectrum_sign,raman_sign):
-        where_zero = torch.eq(raman_sign,torch.zeros_like(raman_sign))
+    def scores(spectrum_sign, raman_sign):
+        where_zero = torch.eq(raman_sign, torch.zeros_like(raman_sign))
         where_not_zero = torch.logical_not(where_zero)
         spectrum_should_zero = spectrum_sign[where_zero]
         spectrum_should_not_zero = spectrum_sign[where_not_zero]
-        true_negative = torch.eq(spectrum_should_zero,torch.zeros_like(spectrum_should_zero)) # 0->0
-        false_positive = torch.logical_not(true_negative)                                     # 0->1
-        false_negative = torch.eq(spectrum_should_not_zero,torch.zeros_like(spectrum_should_not_zero)) # 1->0
-        true_positive = torch.logical_not(false_negative)                                     # 1->1
+        true_negative = torch.eq(
+            spectrum_should_zero, torch.zeros_like(spectrum_should_zero))  # 0->0
+        false_positive = torch.logical_not(
+            true_negative)                                     # 0->1
+        false_negative = torch.eq(spectrum_should_not_zero, torch.zeros_like(
+            spectrum_should_not_zero))  # 1->0
+        true_positive = torch.logical_not(
+            false_negative)                                     # 1->1
         true_negative = true_negative.float().sum()
         false_positive = false_positive.float().sum()
         false_negative = false_negative.float().sum()
         true_positive = true_positive.float().sum()
-        Accuracy = (true_positive+true_negative)/(true_positive+true_negative+false_positive+false_negative)
+        Accuracy = (true_positive+true_negative)/(true_positive +
+                                                  true_negative+false_positive+false_negative)
         Precision = true_positive/(true_positive+false_positive)
         Recall = true_positive/(true_positive+false_negative)
         F1score = 2*Precision*Recall/(Precision+Recall)
         # print(f"A:{Accuracy} P:{Precision} R:{Recall} F:{F1score}")
-        return Accuracy,Precision,Recall,F1score
+        return Accuracy, Precision, Recall, F1score
+
+    @staticmethod
+    def yolov1_loss(raman, predict):
+        confidence_lable = raman[:, :, 0]  # (batch_size, wavelength_clips)
+        batch_size = raman.shape[0]
+        wavelength_clips = raman.shape[1]
+        object_mask = confidence_lable.bool().unsqueeze(
+            dim=2).expand_as(raman)  # (batch_size, wavelength_clips,2)
+        nonobject_mask = torch.logical_not(object_mask)
+        object_target = torch.masked_select(raman, object_mask).reshape(
+            (-1, 2))  # (object_dim ,2)
+        object_predict = torch.masked_select(
+            predict, object_mask).reshape((-1, 2))
+        nonobject_target = torch.masked_select(raman, nonobject_mask).reshape(
+            (-1, 2))  # (nonobject_dim ,2)
+        nonobject_predict = torch.masked_select(
+            predict, nonobject_mask).reshape((-1, 2))
+
+        nonobject_confidence_loss = F.mse_loss(
+            nonobject_predict[:, 0], nonobject_target[:, 0], reduction="sum")
+
+        object_position_loss = F.mse_loss(
+            object_predict[:, 1], object_target[:, 1], reduction="sum")
+
+        # (batch_size, object_dim)
+        position_accuracy = 1 - \
+            F.l1_loss(object_predict[:, 1],
+                      object_target[:, 1], reduction="none")
+        object_confidence_target = object_target[:, 0]*position_accuracy
+        object_confidence_loss = F.mse_loss(
+            object_confidence_target, object_predict[:, 0], reduction="sum")
+
+        loss = 0.1*nonobject_confidence_loss + \
+            object_confidence_loss+5*object_position_loss
+        loss = loss/batch_size
+        return loss
+
+    def loss_scores(self, ramans, predicted_spectrum):
+        raman_confidence = ramans[:, :, 0]
+        predicted_confidence = predicted_spectrum[:, :, 0]
+        loss_trivial = F.l1_loss(predicted_confidence,
+                                 raman_confidence, reduction="none")
+
+        loss_weight = torch.pow(3, raman_confidence)
+        weight_sum = loss_weight.sum(dim=1, keepdim=True)
+        loss_weight = loss_weight/weight_sum
+        loss_weighed = torch.sum(loss_trivial*loss_weight, dim=1).mean()
+
+        predicted_round = torch.round(predicted_confidence)
+        acc, prc, rec, f1 = self.scores(predicted_round, raman_confidence)
+        NaNcheck = torch.logical_or(prc.isnan(), rec.isnan())
+        NaNcheck = torch.logical_or(NaNcheck, f1.isnan())
+        return loss_trivial, loss_weighed, acc, prc, rec, f1, NaNcheck
 
     def training_step(self, batch, batch_idx):
         _, ramans = batch
         predicted_spectrum = self.shared_procedure(batch)
-        loss = F.l1_loss(predicted_spectrum, ramans, reduction="none")
-        self.log("train_loss", loss.mean(), on_epoch=True, on_step=False)
-        raman_sign = torch.sign(ramans)
-        loss_weight = torch.pow(4, raman_sign)
-        weight_sum = loss_weight.sum(dim=1, keepdim=True)
-        loss_weight = loss_weight/weight_sum
-        loss_weighed = torch.sum(loss*loss_weight, dim=1).mean()
+        loss_yolo = self.yolov1_loss(ramans, predicted_spectrum)
+        self.log("train_yolo", loss_yolo, on_epoch=True, on_step=False)
+        loss_trivial, loss_weighed, acc, prc, rec, f1, NaNcheck = self.loss_scores(
+            ramans, predicted_spectrum)
+        self.log("train_loss", loss_trivial.mean(),
+                 on_epoch=True, on_step=False)
         self.log("train_loss_weighed", loss_weighed,
                  on_epoch=True, on_step=False)
-        spectrum_round = torch.round(predicted_spectrum)
-        loss_round = F.l1_loss(spectrum_round, ramans)
-        self.log("train_loss_round", loss_round, on_epoch=True, on_step=False)
-        spectrum_sign = torch.sign(spectrum_round)
-        acc,prc,rec,f1 = self.scores(spectrum_sign,raman_sign)
-        NaNcheck = torch.logical_or(prc.isnan(),rec.isnan())
-        NaNcheck = torch.logical_or(NaNcheck,f1.isnan())
         if NaNcheck.item() == False:
             self.log("train_acc", acc, on_epoch=True, on_step=False)
             self.log("train_prc", prc, on_epoch=True, on_step=False)
             self.log("train_rec", rec, on_epoch=True, on_step=False)
             self.log("train_f1", f1, on_epoch=True, on_step=False)
         else:
-            raise RuntimeError("NaN")
-            # # for layer in self.readout:
-            # #     if hasattr(layer,"reset_parameters"):
-            # #         layer.reset_parameters()
-            # print("Find NaN, triple loss")
-            # loss_weighed = loss_weighed*3
-        return loss_weighed
-
+            # raise RuntimeError("NaN")
+            print("Train:NaN")
+        return loss_yolo
 
     def validation_step(self, batch, batch_idx):
         _, ramans = batch
         predicted_spectrum = self.shared_procedure(batch)
-        loss = F.l1_loss(predicted_spectrum, ramans, reduction="none")
-        self.log("val_loss", loss.mean(), on_epoch=True, on_step=False)
-        raman_sign = torch.sign(ramans)
-        loss_weight = torch.pow(4, raman_sign)
-        weight_sum = loss_weight.sum(dim=1, keepdim=True)
-        loss_weight = loss_weight/weight_sum
-        loss_weighed = torch.sum(loss*loss_weight, dim=1).mean()
+        loss_yolo = self.yolov1_loss(ramans, predicted_spectrum)
+        self.log("val_yolo", loss_yolo, on_epoch=True, on_step=False)
+        loss_trivial, loss_weighed, acc, prc, rec, f1, NaNcheck = self.loss_scores(
+            ramans, predicted_spectrum)
+        self.log("val_loss", loss_trivial.mean(), on_epoch=True, on_step=False)
         self.log("val_loss_weighed", loss_weighed,
                  on_epoch=True, on_step=False)
-        spectrum_round = torch.round(predicted_spectrum)
-        loss_round = F.l1_loss(spectrum_round, ramans)
-        self.log("val_loss_round", loss_round, on_epoch=True, on_step=False)
-        spectrum_sign = torch.sign(spectrum_round)
-        acc,prc,rec,f1 = self.scores(spectrum_sign,raman_sign)
-        NaNcheck = torch.logical_or(prc.isnan(),rec.isnan())
-        NaNcheck = torch.logical_or(NaNcheck,f1.isnan())
         if NaNcheck.item() == False:
             self.log("val_acc", acc, on_epoch=True, on_step=False)
             self.log("val_prc", prc, on_epoch=True, on_step=False)
             self.log("val_rec", rec, on_epoch=True, on_step=False)
             self.log("val_f1", f1, on_epoch=True, on_step=False)
         else:
-            warn("Find NaN")
-        return loss_weighed
+            print("Val:NaN")
+        return loss_yolo
 
     def configure_optimizers(self):
         if self.hparams.optim_type == "AdamW":
@@ -291,8 +323,9 @@ if __name__ == "__main__":
         mode='min',
     )
 
-    train_set = torch.load("materials/JVASP/Train_raman_set_25_uneq.pt")
-    validate_set = torch.load("materials/JVASP/Valid_raman_set_25_uneq.pt")
+    train_set = torch.load("materials/JVASP/Train_raman_set_25_uneq_yolov1.pt")
+    validate_set = torch.load(
+        "materials/JVASP/Valid_raman_set_25_uneq_yolov1.pt")
     train_dataloader = DataLoader(
         dataset=train_set, batch_size=128, num_workers=2, shuffle=True)
     validate_dataloader = DataLoader(
