5b7f554c41276b5b704ed1da491503cb90a4966f
jialanxin <jialanxin1996@hotmail.com>
2021-04-15 17:43:37 +0800

    Finetune: layer 12

--- finetune.py
+++ finetune.py
@@ -1,26 +1,23 @@
-import json
-import argparse
+from os import sync
 import torch
 import torch.nn.functional as F
-import yaml
 import pytorch_lightning as pl
-import warnings
-from warnings import warn
 from pytorch_lightning.loggers import TensorBoardLogger
 from pytorch_lightning.callbacks import ModelCheckpoint
 from torch.utils.data import DataLoader
-from torch.nn import Embedding, RReLU, ReLU, Dropout
 from dataset import StructureRamanDataset
 from pretrain_fmten import Experiment as FmtEn
 from pretrain_spgp import Experiment as SPGP
 from cos_anneal.cosine_annearing_with_warmup import CosineAnnealingWarmupRestarts
+
 
 def ff(input_dim):
     return torch.nn.Sequential(torch.nn.Linear(input_dim, 128))
 
 
 def ff_output(input_dim, output_dim):
-    return torch.nn.Sequential(torch.nn.Linear(input_dim, output_dim) )#, torch.nn.RReLU(), Dropout(0.3), torch.nn.Linear(128, 128), torch.nn.RReLU(), Dropout(0.3), torch.nn.Linear(128, output_dim))
+    # , torch.nn.RReLU(), Dropout(0.3), torch.nn.Linear(128, 128), torch.nn.RReLU(), Dropout(0.3), torch.nn.Linear(128, output_dim))
+    return torch.nn.Sequential(torch.nn.Linear(input_dim, output_dim))
 
 
 class Experiment(pl.LightningModule):
@@ -28,9 +25,10 @@ class Experiment(pl.LightningModule):
         super().__init__()
         self.save_hyperparameters()
         self.lr = lr
-        fmten_model = FmtEn.load_from_checkpoint("pretrain/fmten/epoch=821-step=925571.ckpt")
+        fmten_model = FmtEn.load_from_checkpoint(
+            "pretrain/fmten/epoch=585-step=659835.ckpt")
         spgp_model = SPGP.load_from_checkpoint(
-            "pretrain/spacegroup/epoch=713-step=359855.ckpt")
+            "pretrain/spacegroup/epoch=774-step=872649.ckpt")
         self.atom_embedding = fmten_model.atom_embedding
         self.atomic_number_embedding = fmten_model.atomic_number_embedding
         self.space_group_number_embedding = fmten_model.space_group_number_embedding
@@ -38,21 +36,24 @@ class Experiment(pl.LightningModule):
         self.position_embedding = spgp_model.position_embedding
         self.lattice_embedding = spgp_model.lattice_embedding
         self.encoder = spgp_model.encoder
-        self.re_init_parameters(self.encoder.layers[5])
+        # self.re_init_parameters(self.encoder.layers[5])
         self.readout = ff_output(input_dim=256, output_dim=100)
+
     @staticmethod
     def re_init_parameters(layer):
         for internal_layer in layer.children():
-            if hasattr(internal_layer,"reset_parameters"):
+            if hasattr(internal_layer, "reset_parameters"):
                 print(f"Reset parameters of {internal_layer}")
                 internal_layer.reset_parameters()
-            elif hasattr(internal_layer,"_reset_parameters"):
+            elif hasattr(internal_layer, "_reset_parameters"):
                 print(f"Reset parameters of {internal_layer}")
                 internal_layer._reset_parameters()
+
     @staticmethod
-    def Gassian_expand(value_list, min_value, max_value, intervals, expand_width, device):
+    def Gassian_expand(value_list, min_value, max_value, intervals, expand_width):
         value_list = value_list.expand(-1, -1, intervals)
-        centers = torch.linspace(min_value, max_value, intervals).to(device)
+        centers = torch.linspace(min_value, max_value,
+                                 intervals).type_as(value_list)
         result = torch.exp(-(value_list - centers)**2/expand_width**2)
         return result
 
@@ -75,24 +76,24 @@ class Experiment(pl.LightningModule):
         # (batch_size, max_atoms, 3)
         positions = encoded_graph["positions"]
 
-        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
        # (batch_size, max_atoms, 80)
-        elecneg = self.Gassian_expand(elecneg, 0.5, 4.0, 80, 0.04, device)
+        elecneg = self.Gassian_expand(elecneg, 0.5, 4.0, 80, 0.04)
         # (batch_size, max_atoms, 80)
-        covrad = self.Gassian_expand(covrad, 50, 250, 80, 2.5, device)
+        covrad = self.Gassian_expand(covrad, 50, 250, 80, 2.5)
         # (batch_size, max_atoms, 80)
-        FIE = self.Gassian_expand(FIE, 3, 25, 80, 0.28, device)
+        FIE = self.Gassian_expand(FIE, 3, 25, 80, 0.28)
         # (batch_size, max_atoms, 80)
-        elecaffi = self.Gassian_expand(elecaffi, -3, 3.7, 80, 0.08, device)
+        elecaffi = self.Gassian_expand(elecaffi, -3, 3.7, 80, 0.08)
         # (batch_size, max_atoms, 80)
-        atmwht = self.Gassian_expand(atmwht, 0, 210, 80, 2.63, device)
+        atmwht = self.Gassian_expand(atmwht, 0, 210, 80, 2.63)
         atoms = torch.cat(
             (atoms, elecneg, covrad, FIE, elecaffi, atmwht), dim=2)         # (batch_size, max_atoms, 459)
         atoms = self.atom_embedding(atoms)  # (batch_size,max_atoms,atoms_info)
 
         positions = positions.unsqueeze(dim=3).expand(-1, -1, 3, 80)
-        centers = torch.linspace(-15, 18, 80).to(device)
+        centers = torch.linspace(-15, 18, 80).type_as(positions)
         # (batch_size, max_atoms, 3, 80)
         positions = torch.exp(-(positions - centers)**2/0.41**2)
         # (batch_size, max_atoms, 240)
@@ -112,13 +113,13 @@ class Experiment(pl.LightningModule):
 
         lattice = encoded_graph["lattice"]  # lattice: (batch_size, 9, 1)
         lattice = self.Gassian_expand(
-            lattice, -15, 18, 80, 0.41, device)  # (batch_size, 9, 80)
+            lattice, -15, 18, 80, 0.41)  # (batch_size, 9, 80)
         lattice = torch.flatten(lattice, start_dim=1)  # (batch_size,720)
 
         # lattice: (batch_size,1,1)
         cell_volume = torch.log(encoded_graph["CV"])
         cell_volume = self.Gassian_expand(
-            cell_volume, 3, 8, 80, 0.06, device)  # (batch_size,1,80)
+            cell_volume, 3, 8, 80, 0.06)  # (batch_size,1,80)
         cell_volume = torch.flatten(
             cell_volume, start_dim=1)  # (batch_size, 80)
 
@@ -137,8 +138,8 @@ class Experiment(pl.LightningModule):
         # (1+max_atoms, batch_size, atoms_info)
         atoms = torch.transpose(atoms, dim0=0, dim1=1)
         batch_size = padding_mask.shape[0]
-        cls_padding = torch.zeros((batch_size, 1)).bool().to(
-            device)  # (batch_size, 1)
+        cls_padding = torch.zeros((batch_size, 1)).bool().type_as(
+            padding_mask)  # (batch_size, 1)
 
         # (batch_size, 1+max_atoms)
         padding_mask = torch.cat((cls_padding, padding_mask), dim=1)
@@ -276,17 +277,23 @@ class Experiment(pl.LightningModule):
         _, ramans = batch
         predicted_spectrum = self.shared_procedure(batch)
         loss_yolo = self.yolov1_loss(ramans, predicted_spectrum)
-        self.log("val_yolo", loss_yolo, on_epoch=True, on_step=False)
+        self.log("val_yolo", loss_yolo, on_epoch=True,
+                 on_step=False, sync_dist=True)
         loss_trivial, loss_weighed, acc, prc, rec, f1, NaNcheck = self.loss_scores(
             ramans, predicted_spectrum)
-        self.log("val_loss", loss_trivial.mean(), on_epoch=True, on_step=False)
+        self.log("val_loss", loss_trivial.mean(),
+                 on_epoch=True, on_step=False, sync_dist=True)
         self.log("val_loss_weighed", loss_weighed,
-                 on_epoch=True, on_step=False)
+                 on_epoch=True, on_step=False, sync_dist=True)
         if NaNcheck.item() == False:
-            self.log("val_acc", acc, on_epoch=True, on_step=False)
-            self.log("val_prc", prc, on_epoch=True, on_step=False)
-            self.log("val_rec", rec, on_epoch=True, on_step=False)
-            self.log("val_f1", f1, on_epoch=True, on_step=False)
+            self.log("val_acc", acc, on_epoch=True,
+                     on_step=False, sync_dist=True)
+            self.log("val_prc", prc, on_epoch=True,
+                     on_step=False, sync_dist=True)
+            self.log("val_rec", rec, on_epoch=True,
+                     on_step=False, sync_dist=True)
+            self.log("val_f1", f1, on_epoch=True,
+                     on_step=False, sync_dist=True)
         else:
             print("Val:NaN")
         return loss_yolo
@@ -298,11 +305,12 @@ class Experiment(pl.LightningModule):
         else:
             optimizer = torch.optim.Adam(
                 self.parameters(), lr=self.lr, weight_decay=self.hparams.weight_decay)
-        schedualer = CosineAnnealingWarmupRestarts(optimizer=optimizer,first_cycle_steps=200,max_lr=self.hparams.lr,min_lr=0,warmup_steps=30)
+        schedualer = CosineAnnealingWarmupRestarts(
+            optimizer=optimizer, first_cycle_steps=200, max_lr=self.hparams.lr, min_lr=0, warmup_steps=30)
         return [optimizer], [schedualer]
 
 
-def model_config(optim_type,optim_lr,optim_weight_decay):
+def model_config(optim_type, optim_lr, optim_weight_decay):
     params = {}
     if optim_type == "AdamW":
         params["optim_type"] = "AdamW"
@@ -312,14 +320,15 @@ def model_config(optim_type,optim_lr,optim_weight_decay):
 
 
 if __name__ == "__main__":
-    prefix = "/home/jlx/v0.4.7/8.finetune_linear_output_cos_warmup/"
+    prefix = "/home/jlx/v0.4.7/10.finetune_layer_12/"
     trainer_config = "fit"
     checkpoint_path = None
-    model_hpparams = model_config(optim_type="AdamW",optim_lr=2e-5,optim_weight_decay=1e-2)
+    model_hpparams = model_config(
+        optim_type="AdamW", optim_lr=5e-5, optim_weight_decay=1e-1)
     # train_set_part = 1
     # epochs = 250*train_set_part
-    epochs=2000
-    batch_size=32
+    epochs = 2000
+    batch_size = 128
 
     checkpoint_callback = ModelCheckpoint(
         monitor='val_loss_weighed',
@@ -334,7 +343,6 @@ if __name__ == "__main__":
         dataset=train_set, batch_size=batch_size, num_workers=4, shuffle=True)
     validate_dataloader = DataLoader(
         dataset=validate_set, batch_size=batch_size, num_workers=4)
-
 
     experiment = Experiment(**model_hpparams)
 
